700df53ea5a2ac00211cb41524f8a7b2962ba217
ShellDomenic <domenicaloor177@gmail.com>
2025-01-09 15:34:23 -0500

    cambio1

--- src/main/java/com/mycompany/empresatransporte/EmpresaTransporte.java
+++ src/main/java/com/mycompany/empresatransporte/EmpresaTransporte.java
@@ -3,9 +3,20 @@
 package com.mycompany.empresatransporte;
 
 //clase principal
+
+import com.mycompany.empresatransporte.clases.carro.Avion;
+import com.mycompany.empresatransporte.clases.carro.Carro;
+import com.mycompany.empresatransporte.clases.carro.Moto;
+import com.mycompany.empresatransporte.clases.carro.Vehiculos;
+
 public class EmpresaTransporte {
 
     public static void main(String[] args) {
-        
-    }
+        //crear instancias de las subclases 
+        Vehiculos carro =new Carro();
+        Vehiculos moto =new Moto();
+        Vehiculos avion = new Avion();
+        System.out.println("velocidad "+ carro.calcularDistancia(0, 0));
+       }
+    
 }
--- src/main/java/com/mycompany/empresatransporte/clases/carro/Vehiculos.java
+++ src/main/java/com/mycompany/empresatransporte/clases/carro/Vehiculos.java
@@ -1,9 +1,20 @@
 
 package com.mycompany.empresatransporte.clases.carro;
 
-// clases abstract
+// clases abstract /polimorfismo
 public abstract class  Vehiculos {
     //metodo abstract
     public abstract void transportar (); 
     
+     public double calcularDistancia  (double velocidad, double tiempo){
+             return (velocidad*1);
+       }
+    
+    public double calcularDistancia (double velocidad, double tiempo){
+    return (velocidad * tiempo);
+    
+    }
+    public double calcularDistancia (double velocidad,double tiempo,double constante){
+    return (velocidad * tiempo* constante);
+    }
 }
